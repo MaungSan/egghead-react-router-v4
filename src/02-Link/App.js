8b157da7171cecae0cc6ba0c50a4590c79101bda
cayden <caydenakins@gmail.com>
2018-09-27 14:11:29 -0500

    updated to react 16.4.2

---
+++ src/02-Link/App.js
@@ -0,0 +1,35 @@
+// https://jsbin.com/wemudus
+
+//fix the gaps betwene home about Contact
+
+
+
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+import './App.css'
+
+const Links = () => (
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to={{pathname: '/about'}}>About</Link>
+    <Link replace to="/contact">Contact</Link>
+  </nav>
+)
+
+const App = () => (
+  <Router>
+    <div>
+      <Links />
+      <Route exact path="/" render={() => <h1>Home</h1>} />
+      <Route path="/about" render={() => <h1>About</h1>} />
+      <Route path="/contact" render={() => <h1>Contact</h1>} />
+    </div>
+  </Router>
+);
+
+export default App
